14e407dae705621e8632c44b3fe78c1d031a50c1
Akshaye JH <akshayejh@gmail.com>
2018-03-09 17:27:55 +0530

    Changes for Part 14

--- app/src/main/java/in/tvac/akshayejh/photoblog/BlogRecyclerAdapter.java
+++ app/src/main/java/in/tvac/akshayejh/photoblog/BlogRecyclerAdapter.java
@@ -1,18 +1,35 @@
 package in.tvac.akshayejh.photoblog;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
+import java.util.Date;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapter.ViewHolder> {
 
     public List<BlogPost> blog_list;
+    public Context context;
+
+    private FirebaseFirestore firebaseFirestore;
 
     public BlogRecyclerAdapter(List<BlogPost> blog_list){
 
@@ -24,14 +41,46 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.blog_list_item, parent, false);
+        context = parent.getContext();
+        firebaseFirestore = FirebaseFirestore.getInstance();
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
 
         String desc_data = blog_list.get(position).getDesc();
         holder.setDescText(desc_data);
+
+        String image_url = blog_list.get(position).getImage_url();
+        holder.setBlogImage(image_url);
+
+        String user_id = blog_list.get(position).getUser_id();
+        //User Data will be retrieved here...
+        firebaseFirestore.collection("Users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful()){
+
+                    String userName = task.getResult().getString("name");
+                    String userImage = task.getResult().getString("image");
+
+                    holder.setUserData(userName, userImage);
+
+
+                } else {
+
+                    //Firebase Exception
+
+                }
+
+            }
+        });
+
+        long millisecond = blog_list.get(position).getTimestamp().getTime();
+        String dateString = DateFormat.format("MM/dd/yyyy", new Date(millisecond)).toString();
+        holder.setTime(dateString);
 
     }
 
@@ -45,6 +94,11 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
         private View mView;
 
         private TextView descView;
+        private ImageView blogImageView;
+        private TextView blogDate;
+
+        private TextView blogUserName;
+        private CircleImageView blogUserImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -58,6 +112,35 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
 
         }
 
+        public void setBlogImage(String downloadUri){
+
+            blogImageView = mView.findViewById(R.id.blog_image);
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions.placeholder(R.drawable.image_placeholder);
+            Glide.with(context).applyDefaultRequestOptions(requestOptions).load(downloadUri).into(blogImageView);
+
+        }
+
+        public void setTime(String date) {
+
+            blogDate = mView.findViewById(R.id.blog_date);
+            blogDate.setText(date);
+
+        }
+
+        public void setUserData(String name, String image){
+
+            blogUserImage = mView.findViewById(R.id.blog_user_image);
+            blogUserName = mView.findViewById(R.id.blog_user_name);
+
+            blogUserName.setText(name);
+
+            RequestOptions placeholderOption = new RequestOptions();
+            placeholderOption.placeholder(R.drawable.profile_placeholder);
+
+            Glide.with(context).applyDefaultRequestOptions(placeholderOption).load(image).into(blogUserImage);
+
+        }
 
     }
 
--- app/src/main/java/in/tvac/akshayejh/photoblog/HomeFragment.java
+++ app/src/main/java/in/tvac/akshayejh/photoblog/HomeFragment.java
@@ -9,13 +9,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -31,7 +35,10 @@ public class HomeFragment extends Fragment {
     private List<BlogPost> blog_list;
 
     private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
     private BlogRecyclerAdapter blogRecyclerAdapter;
+
+    private DocumentSnapshot lastVisible;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,7 +46,7 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -47,32 +54,92 @@ public class HomeFragment extends Fragment {
         blog_list = new ArrayList<>();
         blog_list_view = view.findViewById(R.id.blog_list_view);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         blogRecyclerAdapter = new BlogRecyclerAdapter(blog_list);
         blog_list_view.setLayoutManager(new LinearLayoutManager(container.getContext()));
         blog_list_view.setAdapter(blogRecyclerAdapter);
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        if(firebaseAuth.getCurrentUser() != null) {
+
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            blog_list_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    Boolean reachedBottom = !recyclerView.canScrollVertically(1);
+
+                    if(reachedBottom){
+
+                        String desc = lastVisible.getString("desc");
+                        Toast.makeText(container.getContext(), "Reached : " + desc, Toast.LENGTH_SHORT).show();
+
+                        loadMorePost();
+
+                    }
+
+                }
+            });
+
+            Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(3);
+            firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+                    lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() -1);
+
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blog_list.add(blogPost);
+
+                            blogRecyclerAdapter.notifyDataSetChanged();
+
+                        }
+                    }
+
+                }
+            });
+
+        }
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    public void loadMorePost(){
+
+        Query nextQuery = firebaseFirestore.collection("Posts")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
+                .limit(3);
+
+        nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
 
-                for(DocumentChange doc: documentSnapshots.getDocumentChanges()) {
-                    if(doc.getType() == DocumentChange.Type.ADDED){
+                if(!documentSnapshots.isEmpty()) {
 
-                        BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
-                        blog_list.add(blogPost);
+                    lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
 
-                        blogRecyclerAdapter.notifyDataSetChanged();
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blog_list.add(blogPost);
 
+                            blogRecyclerAdapter.notifyDataSetChanged();
+
+                        }
                     }
+
                 }
 
             }
         });
 
-
-        // Inflate the layout for this fragment
-        return view;
     }
 
 }
